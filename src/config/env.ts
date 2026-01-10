dcfc75623b13869d42e1743cd1d19779d2d84238
ViratSathavara <viratmsathavara2510@gmail.com>
2026-01-11 00:14:53 +0530

    changes added

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,5 +6,5 @@ export const ENV = {
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET as string,
   DATABASE_URL: process.env.DATABASE_URL as string,
-  CORS_ORIGINS: process.env.CORS_ORIGINS?.split(",") || ["http://localhost:3000", "http://localhost:3001"],
+  CORS_ORIGINS: process.env.CORS_ORIGINS?.split(",") || ["http://localhost:3000", "http://localhost:3001", "https://devikrupa-web-landing.netlify.app"],
 };
